deb43056d1e65ae4b613fd41c50aa59a068b9ece
Abbey McTaggart <amctagg1@gmail.com>
2019-10-17 20:26:44 -0500

    initial setup wip

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function init() {
+    Tabletop.init( {
+      //key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+      //key: 'https://docs.google.com/spreadsheets/d/1lupfqBIWuExmeB1eDWYkeh0RRCV0nusDmnbpFLLcm6A/pubhtml',
+      key: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS5TxZU8KxXaMkm35HBn_W_pufL9eh3bAHCqY3vw4yCBIgdinWBdCsdbQfijxVGdjAxZsOeSlKFx6O3/pubhtml',
+      simpleSheet: true }
+    ).then(function(data, tabletop) { 
+      console.log(data)
+    })
+  }
+  window.addEventListener('DOMContentLoaded', init)
