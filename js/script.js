c6cf5e9a43b1c49804294dde9aac325b6fd4e973
Abbey McTaggart <amctagg1@gmail.com>
2019-10-19 09:00:06 -0500

    fix sheet URL

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ function init() {
     Tabletop.init( {
       //key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
       //key: 'https://docs.google.com/spreadsheets/d/1lupfqBIWuExmeB1eDWYkeh0RRCV0nusDmnbpFLLcm6A/pubhtml',
-      key: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS5TxZU8KxXaMkm35HBn_W_pufL9eh3bAHCqY3vw4yCBIgdinWBdCsdbQfijxVGdjAxZsOeSlKFx6O3/pubhtml',
+      //key: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS5TxZU8KxXaMkm35HBn_W_pufL9eh3bAHCqY3vw4yCBIgdinWBdCsdbQfijxVGdjAxZsOeSlKFx6O3/pubhtml',
+      key: 'https://docs.google.com/spreadsheets/d/1lupfqBIWuExmeB1eDWYkeh0RRCV0nusDmnbpFLLcm6A/edit?usp=sharing',
       simpleSheet: true }
     ).then(function(data, tabletop) { 
       console.log(data)
