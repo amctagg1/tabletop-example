187e053763938c90a99d7e81fe17ca135135ba2e
Abbey McTaggart <amctagg1@gmail.com>
2019-10-19 09:41:40 -0500

    remove bad keys

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 function init() {
     Tabletop.init( {
-      //key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
-      //key: 'https://docs.google.com/spreadsheets/d/1lupfqBIWuExmeB1eDWYkeh0RRCV0nusDmnbpFLLcm6A/pubhtml',
-      //key: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS5TxZU8KxXaMkm35HBn_W_pufL9eh3bAHCqY3vw4yCBIgdinWBdCsdbQfijxVGdjAxZsOeSlKFx6O3/pubhtml',
       key: 'https://docs.google.com/spreadsheets/d/1lupfqBIWuExmeB1eDWYkeh0RRCV0nusDmnbpFLLcm6A/edit?usp=sharing',
       simpleSheet: true }
     ).then(function(data, tabletop) { 
@@ -10,3 +7,4 @@ function init() {
     })
   }
   window.addEventListener('DOMContentLoaded', init)
+  
