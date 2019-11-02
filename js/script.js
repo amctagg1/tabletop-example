42c45d1e211c4ef56323dd1cebeba3675bc66cf6
Abbey McTaggart <amctagg1@gmail.com>
2019-11-02 10:30:22 -0500

    only write once to page

--- js/script.js
+++ js/script.js
@@ -4,31 +4,63 @@ function init() {
       simpleSheet: true }
     ).then(function(data, tabletop) { 
       //console.log(data)
-      lastPosts(data);
+      postToPage(data);
     })
   }
   window.addEventListener('DOMContentLoaded', init)
   
   
 // Only display the last X posts in the sheet
-var postsToShow = 100;
+var postsToShow = 5;
 
 // go through each row and generate posts to the page
-  function lastPosts(sheet) {
-    var postsEl = document.getElementById('posts');
-    var lastXRows = sheet.slice(-postsToShow);
+  function postToPage(sheet) {
+    // cut down the sheet to only the last X rows
+    var sheetLastX = sheet.slice(-postsToShow);
+
+    var postsEl = document.getElementById('js-posts-container');
+
+    // create the content we'll write to the page
+    var postContent = document.createElement('div');
+    postContent.setAttribute('id', 'js-posts');
     
-    // loop through the last X rows most recent first (ie, bottom up)
-    for (var i = lastXRows.length - 1; i >= 0; --i) {
-      var row = lastXRows[i];
+    // loop through the last X rows most recent first (ie, process the sheet bottom up)
+    for (var i = sheetLastX.length - 1; i >= 0; --i) {
+      var row = sheetLastX[i];
+      // go generate the post html for this row
       var post = generatePost(row);
-      postsEl.innerHTML += post;
+      // add that post to our content
+      postContent.appendChild(post);
     }
+    // write once to the page
+    postsEl.appendChild(postContent);
   }
   
-  function generatePost(row) {
-    var post = '<div>' + row["date-of-announcement"] + '</div>'
-    + '<div>' + row["body-of-announcement"] + '</div>'
-    + '<a href="' + row["link-destination"] + '">' + 'the link' + '</a>';
+  function generatePost(row) { 
+
+    var post = document.createElement('div');
+    post.setAttribute('id', 'js-post');
+
+    var postDate = document.createElement('div');
+    postDate.innerText = row['date-of-announcement'];
+    post.appendChild(postDate);
+
+    var postTitle = document.createElement('div');
+    postTitle.innerText = row['title-of-announcement'];
+    post.appendChild(postTitle);
+
+    var postBody = document.createElement('div');
+    postBody.innerText = row['body-of-announcement'];
+    post.appendChild(postBody);
+
+    var postProduce = document.createElement('div');
+    postProduce.innerText = row['produce-list'];
+    post.appendChild(postProduce);
+
+    var postLink = document.createElement('a');
+    postLink.setAttribute('href', row['link-destination']);
+    postLink.innerText = 'the link';
+    post.appendChild(postLink);
+
     return post;
   };
